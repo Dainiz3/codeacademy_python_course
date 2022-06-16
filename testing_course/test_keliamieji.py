95c1548c0ee04e60f81572cb152abf3f1972c534
Dainiz3 <game4torrent@gmail.com>
2022-06-16 18:27:39 +0300

    Atnaujinta info

---
+++ testing_course/test_keliamieji.py
@@ -0,0 +1,16 @@
+import unittest
+from keliamieji import *
+
+class TestKeliamieji(unittest.TestCase):
+
+    def test_ar_keliamieji(self):
+        rezultatas = ar_keliamieji(2000)
+        lukestis = "Keliamieji"
+        self.assertEqual(lukestis, rezultatas)
+
+if __name__ == "__main__":
+    unittest.main()
+    
+
+# Ran 1 test in 0.007s
+# OK
